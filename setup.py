d6b5e9e571b97eda7668891bf25a245349e13d0d
Callum Walley <callum.walley@nesi.org.nz>
2021-08-24 14:01:33 +1200

    typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="jupter-clara-train-proxy",
     version="0.2.0",
     packages=find_packages(),
-    package_data={"jupyter_clara_train_proxy": ["nvidia_logo.svg", "singularity_wrapper.sh"]},
+    package_data={"jupyter_clara_train_proxy": ["nvidia_logo.png", "singularity_wrapper.sh"]},
     entry_points={
         "jupyter_serverproxy_servers": ["clara = jupyter_clara_train_proxy:setup_clara"]
     }
